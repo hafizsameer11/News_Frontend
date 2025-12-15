74b48e94de6b25ca657a0e9c0a13fc0caadbcb83
hmstech11@gmail.com <146432351+hafizsameer11@users.noreply.github.com>
2025-12-15 13:59:54 +0500

    Update apiConfig.ts

--- src/lib/api/apiConfig.ts
+++ src/lib/api/apiConfig.ts
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1",
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "piro-newsbackend-u5ixau-399336-72-61-22-134.traefik.me",
   TIMEOUT: 30000,
 };
 
